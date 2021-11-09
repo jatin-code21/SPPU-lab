b9d664877c8a13b29faf108c0e433df8311c07d0
Jatin <ozajatin9309@gmail.com>
2021-11-09 14:52:43 +0530

    Sentinal search added

--- DSL GrupB asgn1.py
+++ DSL GrupB asgn1.py
@@ -19,23 +19,37 @@ def binary(rollNo, x):
 
     if (flag == 1):
         print(x, "has attended")
-        print("The index of ",x,"is ",ind)
+        print("The index of ", x, "is ", ind)
     else:
         print(x, "has not attended")
+
+
+def sentinel(rollNo, n, x):
+    last = rollNo[n-1]
+    i = 0
+    rollNo[n-1] = x
+    while (rollNo[i] != x):
+        i = i + 1
+    rollNo[n-1] = last
+    if ((i < n-1) or (x == rollNo[n-1])):
+        print("The rollNo is present at index", i)
+    else:
+        print("The rollNo is not present.")
 
 
 ch = "y"
 n = int(input("Enter the number of students who attended: "))
 rollNo = []
 for i in range(n):
-    print("Enter the rollNo of student",i+1,":", end=" ")
+    print("Enter the rollNo of student", i+1, ":", end=" ")
     roll = int(input())
     rollNo.append(roll)
 while(ch == "y"):
-    
+
     print("Which search method you want to perform: ")
     print("1.Linear Search")
     print("2.Binary Search")
+    print("3.Sentinal Search")
     choice = int(input("Enter the choice number you want to perform: "))
     if (choice == 1):
         x = int(input("Enter the rollNo you want to search: "))
@@ -47,12 +61,16 @@ while(ch == "y"):
                 break
         if (flag == 1):
             print(x, "has attended")
-            print("The index of ",x,"is ",ind)
+            print("The index of ", x, "is ", ind)
         else:
             print(x, "has not attended")
 
     if (choice == 2):
         x = int(input("Enter the rollNo you want to search: "))
         binary(rollNo, x)
+
+    if (choice == 3):
+        x = int(input("Enter the rollNo you want to search: "))
+        sentinel(rollNo, n, x)
 
     ch = input("Want to continue? (y/n): ")
