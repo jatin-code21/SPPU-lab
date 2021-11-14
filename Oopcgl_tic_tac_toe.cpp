4a1673b22bb2a9c466fad7dce26b65b8f0fef335
Jatin <ozajatin9309@gmail.com>
2021-11-15 00:40:45 +0530

    game updated

--- Oopcgl_tic_tac_toe.cpp
+++ Oopcgl_tic_tac_toe.cpp
@@ -24,7 +24,7 @@ int main()
         char psn, choice;
         cout << "Enter the position at which you want to insert O/X: ";
         cin >> psn;
-        cout << "Enter 0/X: ";
+        cout << "Enter O/X: ";
         cin >> choice;
         for (int i = 0; i < 3; i++)
         {
@@ -45,6 +45,90 @@ int main()
                 cout << arr[i][j] << " ";
             }
             cout << nline;
+        }
+
+        // Checking the results
+        if ((arr[0][0] == 'X' && arr[0][1] == 'X' && arr[0][2] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+        else if ((arr[1][0] == 'X' && arr[1][1] == 'X' && arr[1][2] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+        else if ((arr[2][0] == 'X' && arr[2][1] == 'X' && arr[2][2] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][0] == 'X' && arr[1][0] == 'X' && arr[2][0] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][1] == 'X' && arr[1][1] == 'X' && arr[2][1] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][2] == 'X' && arr[1][2] == 'X' && arr[2][2] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][0] == 'X' && arr[1][1] == 'X' && arr[2][2] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][2] == 'X' && arr[1][1] == 'X' && arr[2][0] == 'X'))
+        {
+            cout << "X Player Wins" << nline;
+            break;
+        }
+
+        // For O player
+        else if ((arr[0][0] == 'O' && arr[0][1] == 'O' && arr[0][2] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
+        }
+        else if ((arr[1][0] == 'O' && arr[1][1] == 'O' && arr[1][2] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
+        }
+        else if ((arr[2][0] == 'O' && arr[2][1] == 'O' && arr[2][2] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][0] == 'O' && arr[1][0] == 'O' && arr[2][0] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][1] == 'O' && arr[1][1] == 'O' && arr[2][1] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][2] == 'O' && arr[1][2] == 'O' && arr[2][2] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][0] == 'O' && arr[1][1] == 'O' && arr[2][2] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
+        }
+        else if ((arr[0][2] == 'O' && arr[1][1] == 'O' && arr[2][0] == 'O'))
+        {
+            cout << "O Player Wins" << nline;
+            break;
         }
     }
 }
