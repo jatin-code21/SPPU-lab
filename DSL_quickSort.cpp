ae3fd336bb4a0d2b9fbb373acc19d517cf94a593
Jatin <ozajatin9309@gmail.com>
2021-11-23 02:43:57 +0530

    quickSort algo

---
+++ DSL_quickSort.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nline "\n";
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+    int i = l - 1;
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, r);
+    return i + 1;
+}
+
+void quickSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int pi = partition(arr, l, r);
+        quickSort(arr, l, pi - 1);
+        quickSort(arr, pi + 1, r);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of students: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the marks of student " << i + 1 << " :";
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+    cout << "The top 5 scorer are: " << nline;
+    for (int i = n - 1; i >= n - 5; i--)
+    {
+        cout << arr[i] << " ";
+    }
+}
